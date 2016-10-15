cacff65dea9324a8e0e0ab4b8a0a1b138c7cdf48
lksalbq <lks.albq@gmail.com>
2016-10-14 21:20:41 -0300

    Adjusting server path test

--- config/database.php
+++ config/database.php
@@ -9,11 +9,10 @@ class Database {
     public $conn;
 
     public function __construct(){
-        $this->url =  parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $this->host = $this->url["host"];
-        $this->db_name = substr($this->url["path"], 1);
-        $this->username = $this->url["user"];
-        $this->password = $this->url["pass"];       
+        $this->host = "localhost";
+        $this->db_name = "rc_info";
+        $this->username = "root";
+        $this->password = "";    
     }
     
     public function getConnection() {
